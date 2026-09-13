9d07d26318c8ab41522393db947be0b819afedd1
Michael-D-Angelo <ronaldimaekel@gmail.com>
2026-09-13 14:33:04 +0700

    refactor: improve computer choice logic and enhance human input validation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,8 +4,8 @@ let computerScore = 0;
 
 // SET function computer choice logic
 function getComputerChoice() {
-  let computerChoice = Math.round(Math.random() * 2) + 1;
-  switch (computerChoice) {
+  let computer = Math.round(Math.random() * 2) + 1;
+  switch (computer) {
     case 1:
       return "rock";
     case 2:
@@ -17,8 +17,39 @@ function getComputerChoice() {
 
 // SET function human choice logic
 function getHumanChoice() {
-  const humanChoice = prompt(
-    "Welcome to Rock, paper & scissors\nType your choice...",
-  );
-  return humanChoice;
+  const human = prompt("Welcome to Rock, paper & scissors\nType your choice...")
+    .toLowerCase()
+    .trim();
+  if (human == "rock" || human == "paper" || human == "scissors") {
+    return human;
+  } else {
+    alert("Invalid input!");
+  }
+}
+
+// SET function for single play
+function playRound() {
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  if (
+    (humanSelection == "rock" && computerSelection == "rock") ||
+    (humanSelection == "paper" && computerSelection == "paper") ||
+    (humanSelection == "scissors" && computerSelection == "scissors")
+  ) {
+    return console.log(
+      `It's a Tie!\nPlayer: ${humanSelection}\nComputer: ${computerSelection} `,
+    );
+  } else if (
+    (humanSelection == "rock" && computerSelection == "scissors") ||
+    (humanSelection == "paper" && computerSelection == "rock") ||
+    (humanSelection == "scissors" && computerSelection == "paper")
+  ) {
+    return console.log(
+      `Player wins!\nPlayer: ${humanSelection}\nComputer: ${computerSelection}`,
+    );
+  } else {
+    return console.log(
+      `Computer wins !\nPlayer: ${humanSelection}\nComputer: ${computerSelection}`,
+    );
+  }
 }
